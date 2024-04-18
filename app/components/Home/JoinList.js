37d20d66ea1cd216be66549ed19e95eee4a84fe8
Cristian Zambrano <criszamcham@gmail.com>
2024-04-18 12:40:56 -0700

    create inverted line animation

--- app/components/Home/JoinList.js
+++ app/components/Home/JoinList.js
@@ -4,7 +4,7 @@ import line from "../../assets/decoration/line.svg"
 
 export default function JoinList() {
   return (
-    <section className="flex h-[70vh] p-7 max-w-1440 mx-auto p-20 items-center space-x-28 relative">
+    <section className="flex h-[70vh] max-w-1440 mx-auto p-20 items-center space-x-28 relative">
         <div className='w-1/2'>
             <h2 className='leading-none'>Join our waiting list</h2>
             <p className='mt-1'>Get early access to our app and be the first to experience the future of personalized nutrition</p>
@@ -14,9 +14,7 @@ export default function JoinList() {
             </form>
         </div>
         <figure className="w-1/2">
-          <div className="line-deco">
-            <div className="gradient-spin" />
-          </div>
+          <div className="line-deco"/>
         </figure>
     </section>
   )
